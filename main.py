84adc5bffeda38cd15000c3661a5f275fcec6771
Nic Desilets <nic@desilets.org>
2017-03-22 22:33:20 -0700

    Initial commit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import sys
+import itertools
+import time
 import numpy as np
 
 ################################################################################
@@ -34,24 +36,16 @@ def print_grid(grid):
             sys.stdout.write(str(grid[i][j]) + " ")
         sys.stdout.write('\n')
 
-def print_grid_3d(grid):
-    for i in range(len(grid)):
-        for j in range(len(grid)):
-            values = grid[i][j]
-            for m in range(len(values)):
-                sys.stdout.write(str(values[m]) + '   ')
-        sys.stdout.write('\n')
-
 def get_input_grid(size):
     grid = [[0 for x in range(size)] for y in range(size)]
 
     # Print example 
-    print "Input example: "
-    print "Row [0]: 02340"
+    print "Input example: (for 5x5 grid)"
+    print "Row [0]: 02040"
     print "Row [1]: 00010"
     print "etc...\n"
 
-    print "Enter each row value: "
+    print "Enter row data: "
     # For each row, get row from user
     for i in range(size):
         input_row = raw_input("Row [" + str(i + 1) + "]: ")
@@ -100,42 +94,68 @@ def find_constraints(grid):
     # For each tile 
     for i in range(size):
         for j in range(size):
-            # Get tile value 
+            # Get tile at pos i,j 
             tile_value = grid[i][j]
 
             # If empty tile
             if tile_value == 0:
-                # Find possible values when considering current row and column
+                # Get all values in current row/col of tile
                 row_values = get_row_values(grid, i)
                 col_values = get_col_values(grid, j)
 
+                # Subtract used values from domain
                 used_values = set(row_values).union(set(col_values))
-                possible_values = set(domain).difference(used_values)
+                possible_values = list(set(domain).difference(used_values))
 
-                constrained_grid[i][j].append(list(possible_values))
+                # Add new constraints for tile
+                constrained_grid[i][j] = possible_values
+
             # Else pre-populated, already constrained 
             else:
-                #constrained_grid[i][j].append(tile_value)
-                value = []
+                value = [] # derp
                 value.append(tile_value)
-
-                constrained_grid[i][j].append(value)
+                constrained_grid[i][j] = value
 
     return constrained_grid
 
-def convert_constrained(grid):
-    converted = [[]]
+def calc_candidate_solutions(grid):
+    return list(itertools.product(*grid))
 
-    size = len(grid)
-    for i in range(size):
-        for j in range(size):
-            print "test"
+def find_solution(candidates, size):
+    num_candidates = len(candidates)
 
-def get_possible_solutions(grid):
-    size = len(grid)
-    for i in range(size):
-        for j in range(size):
-            print "test"
+    # For each potential solution 
+    for x in range(num_candidates):
+        # Convert from 1d array to 2d
+        candidate = np.reshape(candidates[x], (-1, size))
+
+        # Assume candidate is valid 
+        valid = True
+
+        for i in range(size):
+            for j in range(size):
+                # Get tile at pos i,j
+                tile = candidate[i][j]
+                row_values = get_row_values(candidate, i)
+                col_values = get_col_values(candidate, j)
+
+                # Remove self from row/col values 
+                row_values.pop(j)
+                col_values.pop(i)
+
+                # Apply game rules 
+                if tile in row_values or tile in col_values:
+                    valid = False
+                    break
+
+            # Skip solution if invalid 
+            if valid is False:
+                break
+        
+        if valid is True:
+            return candidate
+
+    return None
 
 ################################################################################
 # Main
@@ -143,8 +163,8 @@ def get_possible_solutions(grid):
 
 def main():
     grid = [[]]
-    constrained_grid = [[]]
-    permutations = []
+    constrained_grid = [[[]]]
+    permutation = []
     size = 0
 
     # Check arg length
@@ -166,19 +186,36 @@ def main():
     print "\nInput grid: "
     print_grid(grid)
 
-    # --- Shits about to get real 
-
     # Constrain possible values for each tile in input grid 
     print "\nPossible values for each tile: "
     constrained_grid = find_constraints(grid)
-    print_grid_3d(constrained_grid)
+    print_grid(constrained_grid)
 
-    # TODO dont be stupid 
-    # Convert 3d array to 2d array 
-    constrained_grid = convert_constrained(constrained_grid)
+    # Flatten 3d array to 2d array to make life easier 
+    converted_grid = []
+    for i in range(size):
+        for j in range(size):
+            converted_grid.append(constrained_grid[i][j])
 
-    # Generate permutations of constrained input grid 
-    #permutations = get_possible_solutions(constrained_grid)
+    # Generate all potential solutions from constrained grid
+    print "\nCalculating potential solutions..."
+    calct0 = time.time()
+    permutation = calc_candidate_solutions(converted_grid)
+    calct1 = time.time()
+    time_elapsed = (calct1 - calct0)
+    num_solutions = len(permutation)
+    print str(num_solutions) + " solutions generated in " + str(time_elapsed) + " seconds."
+
+    # Find valid solution from all potential solutions 
+    print "\nFinding solution..."
+    calct0 = time.time()
+    solution = find_solution(permutation, size)
+    calct1 = time.time()
+    time_elapsed = (calct1 - calct0)
+    print "Solution found in " + str(time_elapsed) + " seconds."
+
+    print "\nSolution:"
+    print solution
 
 # ---------------------------------------------------------------------------- #
 
